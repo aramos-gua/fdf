90d30f8667bbc43fbe6316c5105b2a0ca3d51723
Alex-Ubuntu-Windows <alejandro.ramos.gua@gmail.com>
2025-04-22 17:55:29 +0100

    Can draw a line and exit with ESC

--- fdf.c
+++ fdf.c
@@ -23,9 +23,14 @@ typedef struct	s_mlx_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-}	t_mlx_data;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
-int	handle_input(int keysym, t_mlx_data *data)
+int	handle_input(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
 	{
@@ -39,9 +44,40 @@ int	handle_input(int keysym, t_mlx_data *data)
 	return (0);
 }
 
+void	ft_put_pixel(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+void ft_draw_line(t_data *data, int x0, int y0, int x1, int y1, int color)
+{
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1;
+    int sy = (y0 < y1) ? 1 : -1;
+    int err = dx - dy;
+
+    while (x0 != x1 || y0 != y1)
+    {
+        ft_put_pixel(data, x0, y0, color);
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
 int	main(void)
 {
-	t_mlx_data	data;
+	t_data	data;
 
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
@@ -53,6 +89,10 @@ int	main(void)
 		free(data.mlx_ptr);
 		return (1);
 	}
+	data.img = mlx_new_image(data.mlx_ptr, WIDTH, HEIGHT);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	ft_draw_line(&data, 50, 50, 100, 50, 0xFFFFFFFF);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img, 0, 0);
 	mlx_key_hook(data.win_ptr, handle_input, &data);
 	mlx_loop(data.mlx_ptr);
 	return (0);
